c9c909ba614d6fbe08eb18497d857954d81e0f25
Mihai Sarbulescu <mihai.sarbulescu@gmail.com>
2020-03-31 06:51:34 +0300

    initial commit

---
+++ WindowsServiceTest/ProjectInstaller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsServiceTest
+{
+	[RunInstaller(true)]
+	public partial class ProjectInstaller : Installer
+	{
+		public string ServiceName { get; set; }
+		public string DisplayName { get; set; }
+		public string Description { get; set; }
+
+		public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+		protected override void OnBeforeInstall(IDictionary savedState)
+		{
+			SetServiceInformation();
+			base.OnBeforeInstall(savedState);
+		}
+
+		protected override void OnBeforeUninstall(IDictionary savedState)
+		{
+			SetServiceInformation();
+			base.OnBeforeUninstall(savedState);
+		}
+
+		private void SetServiceInformation()
+		{
+			if (Context.Parameters.ContainsKey("serviceName") && !string.IsNullOrEmpty(Context.Parameters["serviceName"]))
+			{
+				Context.LogMessage(string.Format("serviceName parameter has been passed in, value: {0}", Context.Parameters["serviceName"]));
+			}
+			else
+			{
+				ServiceName = "Windows Service Test";
+			}
+			if (Context.Parameters.ContainsKey("displayName") && !string.IsNullOrEmpty(Context.Parameters["displayName"]))
+			{
+				Context.LogMessage(string.Format("displayName parameter has been passed in, value: {0}", Context.Parameters["displayName"]));
+			}
+			else
+			{
+				DisplayName = "Windows Service Test Display Name";
+			}
+			if (Context.Parameters.ContainsKey("description") && !string.IsNullOrEmpty(Context.Parameters["description"]))
+			{
+				Context.LogMessage(string.Format("description parameter has been passed in, value: {0}", Context.Parameters["description"]));
+			}
+			else
+			{
+				Description = "Some description here.";
+			}
+		}
+	}
+}
---
+++ WindowsServiceTest/WindowsServiceTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceTest
+{
+    public partial class WindowsServiceTest : ServiceBase
+    {
+        private int eventId = 1;
+
+        private string eventSourceName = "WindowsServiceTest";
+        private string eventLogName = "WindowsServiceTestEventLog";
+
+        public WindowsServiceTest(string[] args)
+        {
+            InitializeComponent();
+
+            if (args.Length > 0)
+            {
+                eventSourceName = args[0];
+            }
+
+            if (args.Length > 1)
+            {
+                eventLogName = args[1];
+            }
+
+            eventLog = new EventLog();
+            if (!EventLog.SourceExists(eventSourceName))
+            {
+                EventLog.CreateEventSource(eventSourceName, eventLogName);
+            }
+            eventLog.Source = eventSourceName;
+            eventLog.Log = eventLogName;
+        }
+
+        ~WindowsServiceTest()
+        {
+            if (EventLog.Exists(eventLogName))
+            { 
+                EventLog.Delete(eventLogName);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("WindowsServiceTest - In OnStart.");
+
+            Timer timer = new Timer
+            {
+                Interval = 10000 // 10 seconds
+
+            };
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("WindowsServiceTest - In OnStop.");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog.WriteEntry("WindowsServiceTest - In OnContinue.");
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            eventLog.WriteEntry("WindowsServiceTest - Monitoring the System", EventLogEntryType.Information, eventId++);
+        }
+    }
+}
